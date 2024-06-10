406e1b72cad0dda00977ab037e1602fe4f50d6e1
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-06-10 20:06:36 +0430

    add authors section

--- src/components/author/Authors.jsx
+++ src/components/author/Authors.jsx
@@ -1,5 +1,49 @@
+import { useQuery } from "@apollo/client";
+import { Avatar, Divider, Grid, Typography } from "@mui/material";
+
+import { GET_AUTHORS_INFO } from "../../graphql/queries";
+import React from "react";
+
 function Authors() {
-  return <div>Authors</div>;
+  const { loading, data, errors } = useQuery(GET_AUTHORS_INFO);
+
+  if (loading) return <h2>Loading ...</h2>;
+
+  if (errors) return <h2>Error ...</h2>;
+
+  console.log(data);
+  const { authors } = data;
+  return (
+    <Grid
+      container
+      sx={{ boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", borderRadius: 4 }}
+    >
+      {authors.map((author, index) => (
+        <React.Fragment key={author.id}>
+          <Grid item xs={12} padding={2}>
+            <a
+              href={`/authors/${author.slug}`}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                textDecoration: "none",
+              }}
+            >
+              <Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />
+              <Typography component="p" variant="p" color="text.secondary">
+                {author.name}
+              </Typography>
+            </a>
+          </Grid>
+          {index !== authors.length - 1 && (
+            <Grid item xs={12}>
+              <Divider variant="middle" />
+            </Grid>
+          )}
+        </React.Fragment>
+      ))}
+    </Grid>
+  );
 }
 
 export default Authors;
