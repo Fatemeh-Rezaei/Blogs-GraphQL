6af4c3eaaf29f8362d586edd4ab70cf28a44163a
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-06-10 23:35:36 +0430

    get aticle details

--- src/components/blog/BlogPage.jsx
+++ src/components/blog/BlogPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+import { GET_POST_INFO } from "../../graphql/queries";
+import Loader from "../shared/Loader";
 
 function BlogPage() {
-  return (
-    <div>BlogPage</div>
-  )
+  const { slug } = useParams();
+
+  const { loading, data, errors } = useQuery(GET_POST_INFO, {
+    variables: {
+      slug,
+    },
+  });
+
+  if (loading) return <Loader />;
+
+  if (errors) return <h4>Errors...</h4>;
+
+  console.log(data)
+
+  return <div>BlogPage</div>;
 }
 
-export default BlogPage
+export default BlogPage;
